1c014613a5aae2863e52430ca1d7175716a7c45a
matos795 <matosalexandre795@gmail.com>
2025-11-28 16:13:31 -0300

    New Route: VisitMain

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './index.css'
+import VisitMain from './routes/VisitMain'
 
 export default function App() {
 
   return (
     <>
-      
+      <VisitMain />
     </>
   )
 }
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import './style.css';
+
+export default function Header() {
+
+    return(
+        <header>
+            <div>
+                <h1>DSCarrosTop</h1>
+            </div>
+        </header>
+    );
+}
