d9276a305b19c8fdc0377b17a0c78a864cb32903
matos795 <matosalexandre795@gmail.com>
2025-11-28 18:16:18 -0300

    Containers in VisitMain

--- src/routes/VisitMain/index.tsx
+++ src/routes/VisitMain/index.tsx
@@ -6,6 +6,16 @@ export default function VisitMain() {
     return(
         <>
             <Header />
+
+            <main>
+                <section id='visit-card-container-section' className='dct-cards-container'>
+                    <h2>Venha nos visitar</h2>
+                </section>
+
+                <section id='visit-comments-container-section'  className='dct-comments-container'>
+                        <h2>O que estão dizendo</h2>
+                </section>
+            </main>
         </>
     );
 }
